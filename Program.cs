e4b483509b324e23505efea9b88318df48745aca
ricRobo4 <artistr3@proton.me>
2022-10-18 09:55:06 -0700

    trying to add number validation

--- Program.cs
+++ Program.cs
@@ -6,13 +6,19 @@
 
 // This bool will decide if the while loop continues
 bool playAgain = true;
+    bool isNumber;
+    bool isNumber2;
+    int num1 = 0;
+    int num2 = 0;
 
 // This while loop will continue looping as long as playAgain is true
 while(playAgain == true)
 {
+    // The program asks the user if they would like to add two numbers, and then takes their input
     Console.WriteLine("Would you like to add two numbers together?");
     Console.Write("YES or NO: ");
     string yesNo = Console.ReadLine();
+    // The user's input will be taken, and converted to all uppercase letters
     yesNo = yesNo.ToUpper();
         if (yesNo == "NO")
         {
@@ -22,8 +28,38 @@ while(playAgain == true)
         else if(yesNo == "YES")
         {
             Console.WriteLine("Alright! Let's begin!");
+            Console.WriteLine(" ");
             Console.Write("Please type your first number: ");
-            int firstNum = Console.ReadLine();
+            string firstNum = Console.ReadLine();
+            //This should confirm if the user's input is a number or not
+            isNumber = Int32.TryParse(firstNum, out Num1);
+
+                if (isNumber == true)
+                {
+                    // This will run if the first number is a valid number
+                    Console.WriteLine(" ");
+                    Console.Write("Now enter your second number:");
+                    string secondNum = Console.ReadLine();
+                    isNumber2 = Int32.TryParse(secondNum, out Num2);
+                    // int num2 = Convert.ToInt32(secondNum);
+
+                    int sum = (num1 + num2);
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Calculating...");
+                    Console.WriteLine(" ");
+                    Console.WriteLine($"Your sum is {sum}.");
+                    Console.WriteLine(" ");
+                }
+                else
+                {
+                    // This will run if it isn't a valid number
+                    Console.WriteLine("Invalid Entry");
+                    Console.Write("Please enter a valid number");
+                }
+
+            // int num1 = Convert.ToInt32(firstNum);
+
+            
         }
         else
         {
